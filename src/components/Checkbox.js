fb07e74156bc033f7b02e640973e0753bdebb67e
Dinesh Kumar CH <105624620+chikkaladinesh@users.noreply.github.com>
2022-07-14 13:48:04 +0530

    Add files via upload

---
+++ src/components/Checkbox.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class checkbox extends Component {
+  state = {
+    checkbox: "",
+    checkboxValid: false,
+    errorMsg: {},
+    selectedCheckBox: 0
+  };
+} validateForm = () => {
+  const { checkboxValid } = this.state;
+  this.setState({
+    formValid: checkboxValid
+  })}
+
+  export default checkbox
